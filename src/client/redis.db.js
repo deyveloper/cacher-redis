ac0ef60c0a8204630a249611fad86d88233bb680
Dav <davidsargsyandev@gmail.com>
2020-09-27 22:46:06 +0400

    Changed redis client host/port parameters to environment variables

--- src/client/redis.db.js
+++ src/client/redis.db.js
@@ -7,6 +7,6 @@ const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
 const REDIS_PORT = process.env.REDIS_PORT || '6379';
 
 // Init redis client
-const client = redis.createClient({ host: 'localhost', port: '6379' });
+const client = redis.createClient({ host: REDIS_HOST, port: REDIS_PORT });
 
 module.exports = client;
